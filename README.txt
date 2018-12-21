9849561a9ae1c595cb902d6eb713ad895143b50c
Jackie Rubly <jrubly@mail.greenriver.edu>
2018-12-21 10:18:23 -0800

    first commit
    first commit!

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jackierubly/pen/NepYzB.
+
+ A simple scratch and win concept using a jQuery scratch plugin.
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('#promo').wScratchPad({
+    size        : 70,       
+    bg:  'http://jennamolby.com/scratch-and-win/images/400.png',
+    realtime    : true, 
+    fg: 'http://jennamolby.com/scratch-and-win/images/overlay.png',
+    'cursor': 'url("http://jennamolby.com/scratch-and-win/images/coin1.png") 5 5, default',
+ });
